925d4acdbdf5b9774fa41f745d3b281de10622fa
Xylem <xylem.pl@gmail.com>
2013-06-24 18:43:52 +0200

    Commit of version 0.1
    * Supports livememe.com, quickmeme.com

---
+++ js/background.js
@@ -0,0 +1,38 @@
+﻿function onMessage(request, sender, sendResponse) {
+    switch (request.type) {
+        case "showIcon":
+            chrome.pageAction.show(sender.tab.id);
+            break;
+    }
+}
+
+function rehostImage(tab) {
+    var clientId = "e3afa33c1e77fad";
+
+    function uploadImage(response) {
+        var url = response.url;
+        
+        if (!url) {
+            return;
+        }
+        
+        $.ajax({
+            dataType: "json",
+            type: "POST",
+            url: "https://api.imgur.com/3/image?image=" + url,
+            headers: {
+                Authorization: "Client-ID " + clientId
+            },
+            success: function (data) {
+                if (data.success && !!data.data.id) {
+                    chrome.tabs.update(tab.id, { url : "http://imgur.com/" + data.data.id } );
+                }
+            }
+        });
+    }
+
+    chrome.tabs.sendMessage(tab.id, { type: "findUrl" }, uploadImage);
+}
+
+chrome.runtime.onMessage.addListener(onMessage);
+chrome.pageAction.onClicked.addListener(rehostImage);
---
+++ js/contentscript.js
@@ -0,0 +1,28 @@
+﻿var handledPages = {
+    livememe: {
+        pattern: /^(?:https?:\/\/)?(?:www\.)?livememe.com\/[a-z0-9]*$/g,
+        url: function () { return $("#memeImage").attr("src"); }
+    },
+    quickmeme: {
+        pattern: /^(?:https?:\/\/)?(?:www\.)?quickmeme.com\/meme\/[a-z0-9]*\/$/g,
+        url: function() { return $("#img").attr("src"); }
+    }
+};
+
+var currentPage;
+
+for (var page in handledPages) {
+    if (handledPages.hasOwnProperty(page) && handledPages[page].pattern.test(window.location.href)) {
+        chrome.runtime.sendMessage({ type: "showIcon" });
+        currentPage = page;
+        break;
+    }
+}
+
+function onMessage(request, sender, sendResponse) {
+    if (request.type === "findUrl") {
+        sendResponse({ url: handledPages[currentPage].url() });
+    }
+}
+
+chrome.runtime.onMessage.addListener(onMessage);
