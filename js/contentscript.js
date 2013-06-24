78f6c2210c3a9bc9aff69efd6364a67474c8a1cf
Xylem <xylem.pl@gmail.com>
2013-06-24 21:59:40 +0200

    Minor fixes
    * Fixed bug where Memegur's icon would show up on Livememe's main page
    * Wrapped content script in an immediately executed function
    * Content script immediately calls noConflict() and uses its local
    jQuery to not conflict with other jQuery versions that may be on the
    page

--- js/contentscript.js
+++ js/contentscript.js
@@ -1,28 +1,32 @@
-﻿var handledPages = {
-    livememe: {
-        pattern: /^(?:https?:\/\/)?(?:www\.)?livememe.com\/[a-z0-9]*$/g,
-        url: function () { return $("#memeImage").attr("src"); }
-    },
-    quickmeme: {
-        pattern: /^(?:https?:\/\/)?(?:www\.)?quickmeme.com\/meme\/[a-z0-9]*\/$/g,
-        url: function() { return $("#img").attr("src"); }
+﻿(function() {
+    var jq = $.noConflict();
+    
+    var handledPages = {
+        livememe: {
+            pattern: /^(?:https?:\/\/)?(?:www\.)?livememe.com\/[a-z0-9]+$/g,
+            url: function () { return jq("#memeImage").attr("src"); }
+        },
+        quickmeme: {
+            pattern: /^(?:https?:\/\/)?(?:www\.)?quickmeme.com\/meme\/[a-z0-9]+\/$/g,
+            url: function() { return jq("#img").attr("src"); }
+        }
+    };
+
+    var currentPage;
+
+    for (var page in handledPages) {
+        if (handledPages.hasOwnProperty(page) && handledPages[page].pattern.test(window.location.href)) {
+            chrome.runtime.sendMessage({ type: "showIcon" });
+            currentPage = page;
+            break;
+        }
     }
-};
 
-var currentPage;
-
-for (var page in handledPages) {
-    if (handledPages.hasOwnProperty(page) && handledPages[page].pattern.test(window.location.href)) {
-        chrome.runtime.sendMessage({ type: "showIcon" });
-        currentPage = page;
-        break;
+    function onMessage(request, sender, sendResponse) {
+        if (request.type === "findUrl") {
+            sendResponse({ url: handledPages[currentPage].url() });
+        }
     }
-}
 
-function onMessage(request, sender, sendResponse) {
-    if (request.type === "findUrl") {
-        sendResponse({ url: handledPages[currentPage].url() });
-    }
-}
-
-chrome.runtime.onMessage.addListener(onMessage);
+    chrome.runtime.onMessage.addListener(onMessage);
+})();
